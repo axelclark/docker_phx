c9d7911db66888597f575848acc6896e32851ed1
Axel Clark <axel.clark@pm.me>
2020-03-08 13:34:49 -0400

    Update failing test for homepage update

--- test/docker_phx_web/controllers/page_controller_test.exs
+++ test/docker_phx_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule DockerPhxWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Docker Phx!"
   end
 end
